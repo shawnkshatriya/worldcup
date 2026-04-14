f0dfec0d4a91c8fa0b32a189efc3c157d1eb5eab
shawnkshatriya <65264265+shawnkshatriya@users.noreply.github.com>
2026-04-13 20:33:44 -0400

    nav broken

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { PlayerProvider } from './hooks/usePlayer'
 import Sidebar from './components/Sidebar'
 import Dashboard from './pages/Dashboard'
